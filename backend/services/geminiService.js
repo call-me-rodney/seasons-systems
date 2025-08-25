0324bac55371ccb2118eeab3bdfd422e59cea5d3
call-me-rodney <kwmrodney@gmail.com>
2025-08-25 21:56:24 +0300

    Fixing ai service

--- backend/services/geminiService.js
+++ backend/services/geminiService.js
@@ -3,12 +3,12 @@ import configs from '../configs/configs.js';
 import logger from '../utils/logger.js';
 
 const genAI = new GoogleGenerativeAI(configs.gemini.apiKey);
-const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash"});
 
 export const generateGeminiResponse = async (prompt) => {
   try {
     const result = await model.generateContent(prompt);
-    const response = await result.response;
+    const response = result.response;
     const text = response.text();
     logger.info('Gemini API call successful');
     return text;
