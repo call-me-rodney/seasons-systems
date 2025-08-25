a70bf73ba08b0d49fce18fa458a6fb9d60780a31
call-me-rodney <kwmrodney@gmail.com>
2025-08-26 02:32:52 +0300

    improving the logger utility

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -3,7 +3,12 @@ import configs from '../configs/configs.js';
 
 const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    winston.format.json()
+  ),
   transports: [
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.File({ filename: 'combined.log' }),
@@ -12,7 +17,13 @@ const logger = winston.createLogger({
 
 if (configs.server.environment !== 'production') {
   logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss'
+      }),
+      winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+    ),
   }));
 }
 
